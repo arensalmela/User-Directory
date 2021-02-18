fbc3bce301d13644b92f8cf42c2e0c0d3162fa91
Aren S <adsalmela@gmail.com>
2021-02-18 17:12:05 -0500

    Pulling more information for each person.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import React, { Component } from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    people: [],
+    search: ""
+  };
+
+  componentDidMount() {
+    console.log("mounted!");
+    const data = fetch("https://randomuser.me/api/?results=50");
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  }
+
+  componentWillUnmount() {
+    console.log("I will unmount");
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleSearchClick = () => {
+    const search = this.state.search;
+    console.log(search)
+    const data = fetch(`https://randomuser.me/api/?gender=${search}`);
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <input
+          type="text"
+          value={this.state.search}
+          onChange={this.handleInputChange}
+        />
+        <button type="button" onClick={this.handleSearchClick}>Search</button>
+        {this.state.people.map((person) => (
+           <tr>
+           <td>{person.name.first + " " + person.name.last}</td>
+           <td>{person.email}</td>
+           <td>{person.cell}</td>
+           <td>{person.location.street + ", " +  person.location.city + ", " + person.location.state}</td>
+         </tr>
+        ))}
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
