6f3a48c32c8ffc7fe1763f07465af39d6d797b28
Aren S <adsalmela@gmail.com>
2021-02-18 21:58:41 -0500

    Table format fix. Headers work.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from "react"
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 export default class App extends Component {
   state = {
     people: [],
-    search: ""
+    keys: ["Name", "Cell Number", "Email", "Location"],
+    search: "",
   };
 
   componentDidMount() {
     console.log("mounted!");
-    const data = fetch("https://randomuser.me/api/?results=50");
+    const data = fetch("https://randomuser.me/api/?results=20");
     data
       .then((response) => response.json())
       .then((response) => {
@@ -28,14 +29,14 @@ export default class App extends Component {
 
   handleSearchClick = () => {
     const search = this.state.search;
-    console.log(search)
+    console.log(search);
     const data = fetch(`https://randomuser.me/api/?gender=${search}`);
     data
       .then((response) => response.json())
       .then((response) => {
         this.setState({ people: response.results });
       });
-  }
+  };
 
   render() {
     return (
@@ -46,20 +47,40 @@ export default class App extends Component {
           value={this.state.search}
           onChange={this.handleInputChange}
         />
-        <button type="button" onClick={this.handleSearchClick}>Search</button>
-        {this.state.people.map((person) => (
-           <tr>
-           <td>{person.name.first + " " + person.name.last}</td>
-           <td>{person.email}</td>
-           <td>{person.cell}</td>
-           <td>{person.location.street + ", " +  person.location.city + ", " + person.location.state}</td>
-         </tr>
-        ))}
+        <button type="button" onClick={this.handleSearchClick}>
+          Search
+        </button>
+
+        <table class="table">
+          <thead style={{ color: "white" }}>
+            {this.state.keys.map((key, index) => {
+              return (
+                <th key={index} onClick={() => this.handleFilterClick(key)}>
+                  {key}
+                </th>
+              );
+            })}
+          </thead>
+          <tbody>
+            {this.state.people.map((person) => (
+              <tr>
+                <td>{person.name.first + " " + person.name.last}</td>
+                <td>{person.email}</td>
+                <td>{person.cell}</td>
+                <td>
+                  {person.location.postcode +
+                    ", " +
+                    person.location.city +
+                    ", " +
+                    person.location.state}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
         <h2>Start editing to see some magic happen!</h2>
       </div>
     );
   }
 }
-
-
-
